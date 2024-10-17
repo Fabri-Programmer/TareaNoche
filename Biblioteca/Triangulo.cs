34c2377c12e8fc2fb31e78b0dd2d16f52e23d0bc
Fabri-Programmer <fabrizziopks@hotmail.com>
2024-10-17 02:35:07 -1100

    NocheTarea

---
+++ Biblioteca/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Triangulo
+    {
+        public void falo()
+        {
+            int a = 0, b = 0, c = 0, total = 0 ;
+            a = 1;
+            b = 2;
+            total = a + b + c;
+
+            Console.WriteLine("El valor es : "+total);
+        }
+        public int valor21(int x=0 , int y=0)
+        {
+            
+            Console.Write("Pon un numero = ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Pon otro numero = ");
+            y=int.Parse(Console.ReadLine());
+            Console.WriteLine("La suma de los numeros es = " + (x + y));
+            return 1;
+            
+        }
+        public int valor222()
+        {
+            int a=0, b=0,total=0;
+            Console.Write("valor = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("valor = ");
+            b= int.Parse(Console.ReadLine());
+            total = a + b;
+            Console.WriteLine("suma es = "+total);
+            return 1;
+
+        }
+    }
+}
---
+++ Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿using Biblioteca;
+
+Triangulo Valor12 = new Triangulo(); // instanciado el objeto 
+
+
+int r;
+string hola = "", hola1;
+do
+{
+    Console.Write("Si o no =  ");
+    hola1 = Console.ReadLine();
+    if (hola1 == "si")
+    {
+        Console.Write("Ingrese nombre = ");
+        hola = Console.ReadLine();
+        if (hola == "Fabri")
+        {
+            Valor12.falo();
+        }
+        if (hola == "Magdiel")
+        {
+            Valor12.valor222();
+        }
+    }
+    if (hola1 == "no")
+    {
+        Console.WriteLine("gracias");
+        break;
+        
+    }
+    
+} while (hola == "si");
+
